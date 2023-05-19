d2cdd03784ec9259fe67efa01d3337534c5b492a
ha213176 <61187538+ha213176@users.noreply.github.com>
2023-05-19 14:58:30 +0800

    Delete display.py

--- display.py
+++
@@ -1,9 +0,0 @@
-import webbrowser    
-import googlemaps
-import time
-
-
-# gmaps=googlemaps.Client(key="這裡輸入你的API KEY")
-
-urL='https://www.google.com/maps/dir/23.5601692,120.469598/台北101%2F世貿'
-webbrowser.get('windows-default').open_new(urL)
