88ac521d49e158d2767b1da9efc3d9e0aa712337
tzuwei <b08611033@ntu.edu.tw>
2023-05-22 17:37:01 +0800

    add text to speech

--- assistant_tzuwei.py
+++ assistant_tzuwei.py
@@ -4,8 +4,9 @@ from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 import speech_recognition as sr
 import csv
-import openai
-import json
+import pyttsx3
+from pydub import AudioSegment
+from pydub.playback import play
 
 def callMap(start="your location", target="台北101"):
     urL='https://www.google.com/maps/dir/' + start + '/' + target
@@ -55,12 +56,41 @@ def langchain(text):
 
     return result[2]
 
+#播放音檔
+def play_audio_file(file_path):
+    audio = AudioSegment.from_file(file_path)
+    play(audio)
+
+
+def text_to_speech(text, output_file=None):
+    # 建立語音合成器物件
+    engine = pyttsx3.init()
+
+    # 設定語音合成引擎的參數
+    engine.setProperty('rate', 150)  # 設定語速，預設為 200
+
+    # 設定中文語音合成引擎
+    engine.setProperty('voice', 'zh')  # 使用中文語音合成引擎
+
+    if output_file:
+        # 將語音合成輸出到音訊檔案
+        engine.save_to_file(text, output_file)
+        engine.runAndWait()
+    else:
+        # 直接播放語音合成結果
+        engine.say(text)
+        engine.runAndWait()
+
 if __name__ == "__main__":
     question = speech_to_text("D:\TzuWei\Interest\side_project\ITG\example1.mp3")
     number = langchain(question)
     data = searchDB(number)
-    callMap(target=data[2])
 
+    text_to_speech(f"屋主名字： {data[1]}，屋主電話： {data[3]}，地址： {data[2]}，將為您開啟地圖", output_file="output.wav")
     print("屋主名字: " + data[1])
     print("屋主電話: " + data[3])
+    print("屋主地址: " + data[2])
+    play_audio_file("output.wav")
+    callMap(target=data[2])
+    
 
